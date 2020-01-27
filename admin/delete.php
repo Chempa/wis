7b104b90dc2185bf5bb625036292f485bd44b0d9
Chempa <30638759+Chempa@users.noreply.github.com>
2020-01-27 15:26:33 +0000

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -1,6 +1,9 @@
 <?php
     include '../db.php';
     include '../user.php';
-    
+    $u = new User();
+    $id = $_GET["user"];
+    $ret = $u->delete($con,$id);
+    header( "Location: page.php?q=0" );
 
 ?>
