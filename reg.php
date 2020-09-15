59a36454ae161141540bfbc067260cfe1fc90b69
Chempa <30638759+Chempa@users.noreply.github.com>
2020-09-15 13:59:46 +0100

    Update reg.php

--- reg.php
+++ reg.php
@@ -44,7 +44,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>WIS - form</title>
+	<title>WIStats - form</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
